98a37dbadb67f75f6b2f5d8e7345057980bfcc9c
randomlulli <72440820+randomlulli@users.noreply.github.com>
2020-11-08 23:08:10 +0100

    Update file with RQ3

--- AMD-HW2.py
+++ AMD-HW2.py
@@ -59,6 +59,7 @@ To calculate rate we can use the number of rows of ds_count_view. They rapresent
     the number of totally view session.'''
 
 complete_funnels_rate = len(ds_count_event)*100/len(ds_count_view)
+print('The funnel rate is: ' + complete_funnels_rate + "%")
 
 '''What’s the operation users repeat more on average within a session?\
  Produce a plot that shows the average number of times users perform\
@@ -101,9 +102,10 @@ avg_purchase = ds_count_purchase['count_purchase'].mean()
 
 '''[RQ2]
     What are the categories of the most trending products overall? \
-    For each month visualize this information through a plot showing the number of sold products per category'''
+    For each month visualize this information through a plot showing the number of sold products per category.\
+        - Plot the most visited subcategories. \n
+        - What are the 10 most sold products per category?'''
 
-# evaluete if is consistent elaborate category step by step  with a dictionary (see test.py)
 
 '''In this request we have considered a subcaegory like a category. A product can be in a category\
      or subcategory (category of category) or can not have a category. \
@@ -111,6 +113,8 @@ avg_purchase = ds_count_purchase['count_purchase'].mean()
     In the output are escluded all nan category.'''
 
 ''' Computating October month'''
+
+'''The top 10 of category with the most popular products'''
 
 q_trending_category_oct = '''select distinct category_id, category_code, count(product_id) as count_product_sold
                         from ds_Oct
@@ -121,7 +125,150 @@ q_trending_category_oct = '''select distinct category_id, category_code, count(p
 ds_trending_category_oct = ps.sqldf(q_trending_category_oct, locals())
 ds_trending_category_oct = ds_trending_category_oct.dropna().head(10)
 
-# for the plot take -1 element in category_code.split('.')
+labels_prod_cat = [e.split('.')[-1] for e in list(ds_trending_category_oct['category_code'])]
+
+plt.figure(figsize=(20, 10))
+plt.bar(labels_prod_cat, ds_trending_category_oct['count_product_sold'])
+plt.xlabel('category')
+plt.ylabel('number of sold product')
+plt.title('Top 10 trending category of october', fontsize=20)
+plt.show()
 
 
+'''The top 10 of category with the highest number of views'''
 
+q_most_viewed_category_oct = '''select distinct category_id, category_code, count(event_type) as count_view
+                        from ds_Oct
+                        where event_type = 'view'
+                        group by category_id, category_code
+                        order by count_view desc;'''
+
+ds_most_viewed_category_oct = ps.sqldf(q_most_viewed_category_oct, locals())
+ds_most_viewed_category_oct = ds_most_viewed_category_oct.dropna().head(10)
+
+labels_most_viewed = [e.split('.')[-1] for e in list(ds_most_viewed_category_oct['category_code'])]
+
+plt.figure(figsize=(20, 10))
+plt.bar(labels_most_viewed, ds_most_viewed_category_oct['count_view'])
+plt.xlabel('category')
+plt.ylabel('number of views')
+plt.title('Top 10 of most viewed category', fontsize=20)
+plt.show()
+
+'''The 10 most sold products for category.\n
+    Is possibile that a category don't have any products in list.'''
+
+categories = set(ds_Oct['category_code'].dropna())
+
+for c in categories:
+    x = ds_Oct[ds_Oct.category_code == c]
+    print('The 10 most sold product for ' + c.split('.')[-1].replace('_', ' '))
+    p = x[x.event_type=='purchase'].loc[:, 'product_id'].value_counts().head(10).to_string()[:]
+    if p != 'Series([], )':
+        print(p)
+
+
+''' Computating November month'''
+
+'''The top 10 of category with the most popular products'''
+
+q_trending_category_nov = '''select distinct category_id, category_code, count(product_id) as count_product_sold
+                        from ds_Nov
+                        where event_type = 'purchase'
+                        group by category_id, category_code
+                        order by count_product_sold desc;'''
+
+ds_trending_category_nov = ps.sqldf(q_trending_category_nov, locals())
+ds_trending_category_nov = ds_trending_category_nov.dropna().head(10)
+
+labels_prod_cat = [e.split('.')[-1] for e in list(ds_trending_category_nov['category_code'])]
+
+plt.figure(figsize=(20, 10))
+plt.bar(labels_prod_cat, ds_trending_category_nov['count_product_sold'])
+plt.xlabel('category')
+plt.ylabel('number of sold product')
+plt.title('Top 10 trending category of november', fontsize=20)
+plt.show()
+
+
+'''The top 10 of category with the highest number of views'''
+
+q_most_viewed_category_nov = '''select distinct category_id, category_code, count(event_type) as count_view
+                        from ds_Nov
+                        where event_type = 'view'
+                        group by category_id, category_code
+                        order by count_view desc;'''
+
+ds_most_viewed_category_nov = ps.sqldf(q_most_viewed_category_nov, locals())
+ds_most_viewed_category_nov = ds_most_viewed_category_nov.dropna().head(10)
+
+labels_most_viewed = [e.split('.')[-1] for e in list(ds_most_viewed_category_nov['category_code'])]
+
+plt.figure(figsize=(20, 10))
+plt.bar(labels_most_viewed, ds_most_viewed_category_nov['count_view'])
+plt.xlabel('category')
+plt.ylabel('number of views')
+plt.title('Top 10 of most viewed category', fontsize=20)
+plt.show()
+
+'''The 10 most sold products for category.\n
+    Is possibile that a category don't have any products in list.'''
+
+categories = set(ds_Nov['category_code'].dropna())
+
+for c in categories:
+    x = ds_Nov[ds_Nov.category_code == c]
+    print('The 10 most sold product for ' + c.split('.')[-1].replace('_', ' '))
+    p = x[x.event_type=='purchase'].loc[:, 'product_id'].value_counts().head(10).to_string()[:]
+    if p != 'Series([], )':
+        print(p)
+
+'''[RQ3]
+    For each category, what’s the brand whose prices are higher on average?'''
+
+q_expensive_brand = '''select distinct category_id, category_code, brand, avg(price) as avg_price
+                        from ds
+                        group by category_id
+                        order by avg_price desc'''
+
+ds_expensive_brand = ps.sqldf(q_expensive_brand, locals()).dropna()
+
+all_cat = set(ds['category_code'].dropna())
+
+for c in all_cat:
+    b = ds_expensive_brand['brand'].loc[ds_expensive_brand['category_code']==c].head(1).to_string().split()[1]
+    if b.isalpha():
+        print(c.split('.')[-1] + ': ' + b)
+
+
+'''Write a function that asks the user a category in input and returns a plot\
+     indicating the average price of the products sold by the brand.'''
+
+'''Make a plot with an only item isn't a good practice.\
+    For a best view plot the average for each category of brand.'''
+
+def brand_status():
+    b = input()
+    d = ds_expensive_brand[ds_expensive_brand.brand==b].dropna()
+    c = [i.split('.')[-1] for i in list(d['category_code'])]
+    plt.figure(figsize=(20, 10))
+    plt.bar(c, d['avg_price'])
+    plt.xlabel('category')
+    plt.ylabel('average price of products')
+    plt.title('Average products price for ' + b, fontsize=20)
+    plt.show()
+
+brand_status()
+
+'''Find, for each category, the brand with the highest average price.\
+     Return all the results in ascending order by price.'''
+    
+'''In this case is necessary print the value of ds_expensive_brand in reserve order.'''
+
+l = []
+for c in all_cat:
+    b = ds_expensive_brand['brand'].loc[ds_expensive_brand['category_code']==c].head(1).to_string().split()[1]
+    if b.isalpha():
+        l += [str(c.split('.')[-1] + ': ' + b)]
+for b in sorted(l, reverse=True):
+    print(b)
